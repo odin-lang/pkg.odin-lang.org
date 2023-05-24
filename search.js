18bd8b4272fc5eaa20e80e760169b37a8648844f
gingerBill <bill@gingerbill.org>
2023-05-24 13:27:03 +0100

    Calculate basic score in fuzzy filter

--- search.js
+++ search.js
@@ -7,16 +7,24 @@ if (odin_search) {
 	const getElementsByClassNameArray = function(x) {
 		return Array.from(document.getElementsByClassName(x));
 	};
-	const fuzzy_filter = function(hay, s) {
-		hay = hay.toLowerCase();
-		s = s.toLowerCase();
-		var i = 0, n = -1, l;
-		for (;l = s[i++];) {
-			if (!~(n = hay.indexOf(l, n+1))) {
-				return false;
+	const fuzzy_filter = function(str, pattern) {
+		str = str.toLowerCase();
+		pattern = pattern.toLowerCase();
+
+		var i = 0, n = -1, score = 0, l, run = 0;
+		for (;l = pattern[i++];) {
+			if (!~(n = str.indexOf(l, n+1))) {
+				score -= 10;
+				run = 0;
+			} else {
+				run += 1;
+				score += run*10;
 			}
 		}
-		return true;
+		if (l) {
+			score -= l.length;
+		}
+		return score;
 	};
 
 	let entities = odin_pkg_data.entities.map(e => e.name);
@@ -40,34 +48,42 @@ if (odin_search) {
 		setAllDisplays(null);
 	};
 
+	let curr_search_value = "";
+
 	odin_search.addEventListener("input", ev => {
-		if (odin_search.value) {
-			let search_text = odin_search.value.trim();
+		let search_text = odin_search.value.trim();
+		if (curr_search_value != search_text) {
+			curr_search_value = search_text;
+			if (search_text) {
+				let results_e_and_score = entities.map(e => [e, fuzzy_filter(e, search_text)]).filter(v => v[1] >= 0);
+				let results = results_e_and_score.map(v => v[0]);
+				let scores  = results_e_and_score.map(v => v[1]);
+				let max_score = Math.max(...scores);
+				scores = scores.map(s => max_score-s);
 
-			let results = entities.filter(e => fuzzy_filter(e, search_text));
-			if (results.length) {
-				setAllDisplays("none");
-
-				let i = 0;
-				for (const e of doc_entities) {
-					let name = e.getElementsByTagName("h3")[0].id;
-					if (results.includes(name)) {
-						e.style.display = null;
-						e.style.order = i++;
-					} else {
-						e.style.display = "none";
-						e.style.order = null;
+				if (results.length) {
+					setAllDisplays("none");
+					for (const e of doc_entities) {
+						let name = e.getElementsByTagName("h3")[0].id;
+						let idx = results.indexOf(name);
+						if (idx >= 0) {
+							e.style.display = null;
+							// e.style.order = scores[idx];
+						} else {
+							e.style.display = "none";
+							// e.style.order = null;
+						}
 					}
+				} else {
+					resetEntities();
 				}
 			} else {
 				resetEntities();
 			}
-		} else {
-			resetEntities();
 		}
 		ev.stopPropagation();
 		return;
-	} , false);
+	}, false);
 }
 
 
